5630c07de005335e086e5cff1bf1c598cafeb9ff
Quentin Guenot <quentin.guenot@viacesi.fr>
2020-05-29 11:11:33 +0200

    initial commit

---
+++ test.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading;
+
+
+
+public class CLpara {
+
+    int count;
+
+
+    public void method_para {
+        Thread.Sleep(1000);
+        console.writelineNewStruct;
+        
+        for (count = 0; count < 10; count = count++)
+    }
+
+static void main() => method_para();
+
+    
+}
+
+main();
+
